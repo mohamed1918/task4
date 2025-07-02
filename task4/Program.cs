1e2b49edb89403b14ff0c330cc26dc833d906b26
mohamed1918 <memoohossam2004@gmail.com>
2025-07-03 00:41:16 +0300

    Q2

--- task4/Program.cs
+++ task4/Program.cs
@@ -7,16 +7,24 @@
             a = 10; // This change won't affect the original variable
         }
 
-        
+        static void ByReference(ref int a)
+        {
+            a = 20; // This change will affect the original variable
+        }
+
         static void Main()
         {
             #region Q1
-            int x = 5;
-            Byvalue(x);
-            Console.WriteLine($"After Byvalue: {x}"); // Output: 5 Not changed
+            //int x = 5;
+            //Byvalue(x);
+            //Console.WriteLine($"After Byvalue: {x}"); // Output: 5 Not changed
             #endregion
 
-          
+            #region Q2
+            int y = 5;
+            ByReference(ref y);
+            Console.WriteLine($"After ByReference: {y}"); // Output: 20 Changed 
+            #endregion
         }
     }
 }
