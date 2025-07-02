cfd27f7402b580a75314a84995fcbb382e329767
mohamed1918 <memoohossam2004@gmail.com>
2025-07-03 00:56:38 +0300

    Q3

--- task4/Program.cs
+++ task4/Program.cs
@@ -12,6 +12,14 @@
             a = 20; // This change will affect the original variable
         }
 
+        static void sumAndsub(int a, int b,int c,int d, out int sum, out int sub)
+        {
+            sum = a + b;
+            sub = a - b;
+        }
+
+        
+
         static void Main()
         {
             #region Q1
@@ -21,10 +29,24 @@
             #endregion
 
             #region Q2
-            int y = 5;
-            ByReference(ref y);
-            Console.WriteLine($"After ByReference: {y}"); // Output: 20 Changed 
+            //int y = 5;
+            //ByReference(ref y);
+            //Console.WriteLine($"After ByReference: {y}"); // Output: 20 Changed 
             #endregion
+
+            #region Q3
+            Console.WriteLine("Enter 4 numbers : ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            sumAndsub(a, b, c, d, out int sum, out int sub);
+            Console.WriteLine($"sum = {sum} ");
+            Console.WriteLine($"sub = {sub} ");
+            #endregion
+
+
         }
     }
 }
